cafcd7b4e1a6cc1dc165a6f4d2d1e39b65832eff
Denys Bystryi <bystryydenis211999@gmail.com>
2024-06-30 13:40:22 +0300

    .

---
+++ src/js/1-timer.js
@@ -0,0 +1,5 @@
+// Описаний в документації
+import flatpickr from 'flatpickr';
+// Додатковий імпорт стилів
+import 'flatpickr/dist/flatpickr.min.css';
+console.log(flatpickr); // Додайте цей рядок, щоб перевірити, чи правильно імпортовано бібліотеку
